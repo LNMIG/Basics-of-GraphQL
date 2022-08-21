ef98c16df3b4934906c0a94a7914a457e0e89700
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-21 19:30:37 -0300

    DOCS: files modified

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { graphqlHTTP } from 'express-graphql'
 import express from 'express'
 import { readFileSync } from 'fs'
 import { join } from 'path'
-import { resolvers } from './lib/rootValues.js'
+import { resolvers } from './lib/resolvers.js'
 
+dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -15,7 +17,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 app.use('/api', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: process.env.NODE_ENV === 'true'
 }))
 
 app.listen(port, () => {
