b8728f81258486920baa8f002e50a16f947dc3a2
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-23 09:37:39 -0300

    DOCS: cors added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { graphqlHTTP } from 'express-graphql'
+import cors from 'cors'
 import express from 'express'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 import { resolvers } from './lib/resolvers.js'
 
 dotenv.config()
+const isDev = process.env.NODE_ENV.trim() !== 'production'
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -14,10 +16,11 @@ const typeDefs = readFileSync( join( './', 'lib', 'schema.gql'),'utf-8' )
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+app.use(cors())
 app.use('/api', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: process.env.NODE_ENV === 'true'
+    graphiql: isDev
 }))
 
 app.listen(port, () => {
